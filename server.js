9e5f87e58516a09b9842952cb0d478d94265534e
kaizvn <hungnh1704@gmail.com>
2015-04-06 12:25:29 +0700

    update server config
    test on deployment

--- server.js
+++ server.js
@@ -6,10 +6,15 @@
 
 var express = require('express')
     , app = express()
+    , http = require('http').Server(app)
     , bodyParser = require('body-parser')
-    , io = require('socket.io')(8282)
+    , io = require('socket.io')(http)
     , repository = require('./app/modules/controllers/RepositoryCtrl');
 
+http.listen(8282, '0.0.0.0', function () {
+    var sv = http.address();
+    console.log('socket started at %s:%s', sv.address, sv.port);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,5 +35,5 @@ app.use(express.static('public'));
 
 var server = app.listen('8080', '0.0.0.0', function () {
     var sv = server.address();
-    console.log('server connected at %s:%s', sv.address, sv.port);
+    console.log('server started at %s:%s', sv.address, sv.port);
 });
